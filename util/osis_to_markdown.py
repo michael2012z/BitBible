a794fa3c209d87afbfbaa88cd03c3a89991ba580
Michael Zhao <michael2012zhao@hotmail.com>
2020-02-16 04:43:31 +0000

    readme

--- util/osis_to_markdown.py
+++ util/osis_to_markdown.py
@@ -112,11 +112,12 @@ def generate_readme_books_table(testament_abbrev, readme_items):
             readme_item = list(filter(lambda x: x[0] == long_name, readme_items))[0]
 
             # show 10 chapter-links for the most
+            num_chapter_links = 8
             chapter_numbers = list(range(1, readme_item[2] + 1))
             selected_chapter_numbers = chapter_numbers
-            if readme_item[2] > 10:
-                number_in_group = readme_item[2] / 10
-                selected_chapter_numbers = list(map(lambda x: chapter_numbers[int(x * number_in_group)], list(range(10))))
+            if readme_item[2] > num_chapter_links:
+                number_in_group = readme_item[2] / num_chapter_links
+                selected_chapter_numbers = list(map(lambda x: chapter_numbers[int(x * number_in_group)], list(range(num_chapter_links))))
                 
             for i in selected_chapter_numbers:
                 link = short_name + '.md#' + (long_name + ' ' + str(i)).replace(' ', '-').lower()
