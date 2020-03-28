fe64f32ddabd47c578d2274b2a405810873816f0
Michael Zhao <michael.zhao@arm.com>
2020-03-28 18:21:28 +0800

    Add setup script.

---
+++ util/dictionary/raw_data/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat Vocab*.zip > whole.zip
+zip -FF whole.zip --out Vocabulary.zip
+unzip Vocabulary.zip
+cat Vocabulary/x* > Vocabulary.xml
+rm -f whole.zip
+rm -f Vocabulary.zip
+rm -rf Vocabulary
+
+cat Collin*.zip > whole.zip
+zip -FF whole.zip --out Collins.zip
+unzip Collins.zip
+cat Collins/x* > Collins.xml
+rm -f whole.zip
+rm -f Collins.zip
+rm -rf Collins
